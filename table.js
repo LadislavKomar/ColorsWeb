78ced546e6ff1019c3695af9b8cdfa10e72f3a04
LadislavKomar <komar.ladislav@gmail.com>
2024-10-10 10:36:13 +0200

    Initial commit

---
+++ table.js
@@ -0,0 +1,11 @@
+fetch("./color_default.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const root = document.getElementById("tableContent");
+    data.colors.forEach((element) =>
+      root.insertAdjacentHTML(
+        "beforebegin",
+        `<tr><td>${element.color}</td><td>${element.type}</td><td>${element.code.rgba[0]}</td><td>${element.code.rgba[1]}</td><td>${element.code.rgba[2]}</td></tr>`
+      )
+    );
+  });
